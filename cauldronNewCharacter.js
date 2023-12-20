79bf53de5c88db02ce2b40b17ed2b628bc89beea
Jamster3000 <148760154+Jamster3000@users.noreply.github.com>
2023-12-20 22:30:51 +0000

    Add files via upload

--- cauldronNewCharacter.js
+++ cauldronNewCharacter.js
@@ -1,16 +1,18 @@
 // contentScript.js
 
-console.log('Content script is running.');
-
 // Wait for the page to fully load by using setTimeout
 setTimeout(function () {
-    console.log('Attempting to modify the page.');
-
     // Attempt to find the input element with id "name" on the page
     const nameInput = document.getElementById('name');
     const hitPoints = document.getElementById('hitpoints');
     const armourClass = document.getElementById('armor_class');
     const initiative = document.getElementById('initiative');
+    
+    const newButton = document.createElement('button');
+    newButton.textContent = 'Auto Fill';
+    newButton.classList.add('btn', 'btn-default');
+    const btnGroup = document.querySelector('.btn-group'); 
+    btnGroup.appendChild(newButton);
 
     if (nameInput) {
         const addNameButton = createButton('Add Character Name');
@@ -45,7 +47,7 @@ setTimeout(function () {
                     const level = calculateLevel(characterData.currentXp)
                     const totalHitPoints = characterData.baseHitPoints + (Number(conStat) * Number(level));//constitustion modifier X level + base hit points = total hp
                     hitPoints.value = totalHitPoints;
-                    console.log(conStat, level, totalHitPoints);
+                    console.log(totalHitPoints);
                 } else {
                     console.log('Character Data not found in storage');
                 }
@@ -60,31 +62,94 @@ setTimeout(function () {
 
             chrome.storage.local.get('characterData', function (result) {
                 const characterData = result.characterData;
-
+		console.log(characterData);
                 // Check if characterData and classFeatures are defined
-                if (characterData && characterData.classFeatures) {
-                    const armourClass = characterData.armourClass;
+                if (characterData && characterData.classes[0].classFeatures) {
+                    const armourClassData = characterData.armourClass;	   
+		    totalArmourClass = 0;
+		    const inventory = characterData.inventory;
 
-                    // Check if armourClass is null
-                    if (armourClass == null) {
-                        //class features
-                        const features = characterData.classFeatures;
-                        const unarmoredDefense = features.find(feature => feature.name === "Unarmored Defense");
-
-                        if (unarmoredDefense) {
-                            console.log("Has Unarmored Defense");
-                            console.log(unarmoredDefense.description);
-                        } else {
-                            console.log("Doesn't have Unarmored Defense");
-                        }
-                    }
-                }
+		    //looks for armour that is equipped
+		    for (let i = 0; i < inventory.length; i++) {
+			if (inventory[i].definition.armorClass != null) {
+			    totalArmourClass = inventory[i].definition.armorClass;
+			    armourClass.value = totalArmourClass;
+			    console.log("armour found");
+			    break;
+			} else {
+			    if (totalArmourClass === 0) {
+     			        const features = characterData.classes[0].classFeatures;
+			        const currentIteratedFeature = features[i]
+				if (currentIteratedFeature.definition.name ===  "Unarmored Defense" || currentIteratedFeature.definition.name === "Natural Armor") {
+			 	    //the unarmored defense gives additional AC without the need for armour
+				    if (characterData.classes[0].definition.name === "Monk") {
+				        const dexStat = Math.floor((characterData.stats[1].value - 10)/2);
+				        const wisStat = Math.floor((characterData.stats[4].value - 10)/2);
+				        const armourClassWorkedTotal = 10 + dexStat + wisStat;
+				        armourClass.value = armourClassWorkedTotal;
+				        break;
+				    } else if (characterData.classes[0].definition.name === "Barbarian") {
+			                const dexStat = Math.floor((characterData.stats[1]['value'] - 10)/2)//(dex total -10) /2
+				        const conStat = Math.floor((characterData.stats[2]['value'] - 10)/2)
+				        const armourClassWorkedTotal = 10 + dexStat + conStat;
+				        armourClass.value = armourClassWorkedTotal;
+				        break;
+				    } else if (characterData.race.baseName === "Lizardfolk") {
+			                const dexStat = Math.floor((characterData.stats[1]['value'] - 10)/2)//(dex total -10) / 2
+				        const armourClassWorkedTotal = 13 + dexStat;
+				        armourClass.value = armourClassWorkedTotal;
+				        break;
+				    } else if (characterData.classes[0].definition.name === "Sorcerer" && characterData.classes[0].subClassDefinition.name === "Draconic") {
+				        const dexStat = Math.floor((characterData.stats[1]['value'] - 10)/2)//(dex total -10) / 2
+				        const armourClassWorkedTotal = 13 + dexStat;
+				        armourClass.value = armourClassWorkedTotal;
+				        break;
+				    } else if (characterDara.race.baseName === "Locathah") {
+				        const dexStat = Math.floor((characterData.stats[1]['value'] - 10)/2)//(dex total -10) / 2
+				        const armourClassWorkedTotal = 12 + dexStat;
+				        armourClass.value = armourClassWorkedTotal;
+				        break;
+				    } else if (characterData.race.baseName === "Loxodon") {
+				        const dexStat = Math.floor((characterData.stats[1]['value'] - 10)/2)//(dex total -10) / 2
+				        const armourClassWorkedTotal = 13 + dexStat;
+				        armourClass.value = armourClassWorkedTotal;
+				        break;
+				    } else if (characterData.race.baseName === "tortle") {
+				        const dexStat = Math.floor((characterData.stats[1]['value'] - 10)/2)//(dex total -10) / 2
+				        const armourClassWorkedTotal = 13 + dexStat;
+				        armourClass.value = armourClassWorkedTotal;
+				        break;
+				    }
+				}
+			    }
+			}
+		    }
+		}		    
             });
         });
 
         armourClass.after(addArmourClass);
     }
+    if (initiative) {
+        const addInitiativeButton = createButton('Add Initiative');
+        initiative.after(addInitiativeButton);
 
+        addInitiativeButton.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            chrome.storage.local.get('characterData', function (result) {
+                const characterData = result.characterData;
+                if (characterData) {
+                  
+                    const dexStat = Math.ceil((characterData.stats[1].value - 10)/2)//ability score -10/2 to get modifier (round up)
+                    initiative.value = dexStat;
+                } else {
+                    console.log('Character Data not found in storage');
+                }
+            })
+        });
+        initiative.after(addInitiativeButton);
+    }
     else {
         console.log('Name input not found.');
     }
